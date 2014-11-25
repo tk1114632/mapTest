1b4899aee24b7b1ea39861553b759f575421ba50
tk1114632 <smh91@163.com>
2014-11-26 01:37:49 +0800

    UI company completion by Tingyang & Connected DB

--- src/com/example/mapTest/DBManager.java
+++ src/com/example/mapTest/DBManager.java
@@ -25,12 +25,11 @@ public class DBManager {
         db.beginTransaction();
         try {
             for (ContactInfo person : personList) {
-                //Log.e("=============!!!!!!!!!!!!!!!!", "EEca");
                 db.execSQL("INSERT INTO Person(name,company,addr,position_lat,position_lng,tel) VALUES(?, ?, ?, ?, ?, ?)", new Object[]{person.getName()
                                     , person.getCompany(), person.getAddress(), person.getPositionLat()
                                     , person.getPositionLng(), person.getTel()});
-                Cursor c = db.rawQuery("SELECT _id FROM Person WHERE name=?", new String[]{person.getName()});
-                person.setDb_id(c.getColumnIndex("_id"));
+                //Cursor c = db.rawQuery("SELECT _id FROM Person WHERE name=?", new String[]{person.getName()});
+                //person.setDb_id(c.getColumnIndex("_id"));
             }
             db.setTransactionSuccessful();
         } finally {
@@ -83,10 +82,10 @@ public class DBManager {
                 newPerson.setPositionLat(c.getDouble((c.getColumnIndex("position_lat"))));
                 newPerson.setPositionLng(c.getDouble((c.getColumnIndex("position_lng"))));
                 newPerson.setTel(c.getString((c.getColumnIndex("tel"))));
+                newPerson.setDb_id(c.getInt((c.getColumnIndex("_id"))));
                 currentList.add(newPerson);
             }
         }
-
         c.close();
         return currentList;
     }
@@ -128,6 +127,104 @@ public class DBManager {
         c.close();
         return currentList;
     }
+
+    //The Methods for the Table "Company"
+    public void add_company(ArrayList<CompanyInfo> CompanyList){
+        db.beginTransaction();
+        try {
+            for (CompanyInfo company : CompanyList) {
+                //Log.e("=============!!!!!!!!!!!!!!!!", "EEca");
+
+                db.execSQL("INSERT INTO Company(name, address, phone, website,billing_address_id, contact_id, event_id," +
+                        "industry_id, sale_id, shipping_address_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", new Object[]{
+                        company.getName(),company.getAddress(),company.getPhone(),company.getWebsite(),
+                        company.getBilling_address_id(),company.getContact_id(),company.getEvent_id(),company.getIndustry_id(),
+                        company.getSale_id(),company.getShipping_address_id()
+                });
+                //Cursor c = db.rawQuery("SELECT _id FROM Company WHERE name=?", new String[]{company.getName()});
+                //company.setDb_id(c.getColumnIndex("_id"));
+
+                //Debug 信息
+                //Log.e("Company name:" + c.getString(c.getColumnIndex("name")), " ============");
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+    }
+    public void company_updateName (CompanyInfo company){
+        ContentValues cv = new ContentValues();
+        cv.put("name", company.getName());
+        db.update("Company", cv, "name = ?", new String[]{company.getName()});
+    }
+
+    public void company_updatePhone (CompanyInfo company){
+        ContentValues cv = new ContentValues();
+        cv.put("phone", company.getPhone());
+        db.update("Company", cv, "phone = ?", new String[]{company.getPhone()});
+    }
+    public void company_updateWebsite (CompanyInfo company){
+        ContentValues cv = new ContentValues();
+        cv.put("website", company.getWebsite());
+        db.update("Company", cv, "website = ?", new String[]{company.getWebsite()});
+    }
+
+
+    public void deleteOldCompany(CompanyInfo company){
+        db.delete("Company", "name=?", new String[]{company.getName()});
+    }
+
+    public void company_deleteAll() {
+        db.execSQL("DELETE FROM Company");
+    }
+
+    public ArrayList<CompanyInfo> company_queryAll() {
+        ArrayList<CompanyInfo> currentList = new ArrayList<CompanyInfo>();
+        Cursor c = db.rawQuery("SELECT * FROM Company", null);
+        if (c != null) {
+            while (c.moveToNext()) {
+                CompanyInfo newCompany =  new CompanyInfo();
+                newCompany.setName(c.getString((c.getColumnIndex("name"))));
+                newCompany.setPhone(c.getString((c.getColumnIndex("phone"))));
+                newCompany.setWebsite(c.getString((c.getColumnIndex("website"))));
+                newCompany.setBilling_address_id(c.getInt((c.getColumnIndex("billing_address_id"))));
+                newCompany.setContact_id(c.getInt((c.getColumnIndex("contact_id"))));
+                newCompany.setEvent_id(c.getInt((c.getColumnIndex("event_id"))));
+                newCompany.setIndustry_id(c.getInt(c.getColumnIndex("industry_id")));
+                newCompany.setSale_id(c.getInt(c.getColumnIndex("sale_id")));
+                newCompany.setShipping_address_id(c.getInt(c.getColumnIndex("shipping_address_id")));
+
+                currentList.add(newCompany);
+            }
+        }
+
+        c.close();
+        return currentList;
+    }
+
+    public ArrayList<CompanyInfo> company_queryName(String key_name) {
+        ArrayList<CompanyInfo> currentList = new ArrayList<CompanyInfo>();
+        Cursor c = db.rawQuery("SELECT * FROM  Company WHERE name = ?", new String[]{key_name});
+        if (c != null) {
+            while (c.moveToNext()) {
+                CompanyInfo newCompany =  new CompanyInfo();
+                newCompany.setName(c.getString((c.getColumnIndex("name"))));
+                newCompany.setPhone(c.getString((c.getColumnIndex("phone"))));
+                newCompany.setWebsite(c.getString((c.getColumnIndex("website"))));
+                newCompany.setBilling_address_id(c.getInt((c.getColumnIndex("billing_address_id"))));
+                newCompany.setContact_id(c.getInt((c.getColumnIndex("contact_id"))));
+                newCompany.setEvent_id(c.getInt((c.getColumnIndex("event_id"))));
+                newCompany.setIndustry_id(c.getInt(c.getColumnIndex("industry_id")));
+                newCompany.setSale_id(c.getInt(c.getColumnIndex("sale_id")));
+                newCompany.setShipping_address_id(c.getInt(c.getColumnIndex("shipping_address_id")));
+
+                currentList.add(newCompany);
+            }
+        }
+        c.close();
+        return currentList;
+    }
+    ////////////////////////////////////////////////////////////////////////////////
 
     //close database
     public void closeDB() {
--- src/com/example/mapTest/main.java
+++ src/com/example/mapTest/main.java
@@ -30,9 +30,12 @@ public class main extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_company);
 
         ContactDBManager = new DBManager(this);
+    }
+
+        /*ContactDBManager = new DBManager(this);
 
         refreshList();
 
@@ -92,7 +95,209 @@ public class main extends Activity {
 
     public void refreshButton(View view){
         refreshList();
+    }*/
+
+    public void refreshCompanyList(){
+        //ArrayList<String> arrForList = new ArrayList<String>();
+        ListView list1 = (ListView) findViewById(R.id.companies_List);
+        ArrayList<CompanyInfo> contentInDB = new ArrayList<CompanyInfo>();
+        contentInDB = ContactDBManager.company_queryAll();
+        //for (CompanyInfo currentContact : contentInDB) {
+        //    arrForList.add(currentContact.getName());
+        //}
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, arrForList);
+        Context context = getApplicationContext();
+        companyListAdapter arrayAdapter = new companyListAdapter(context, contentInDB);
+        list1.setAdapter(arrayAdapter);
+
     }
+
+    public void refreshCompanyButton(View view) {
+        refreshCompanyList();
+    }
+
+    public void toMapViewButton(View view){
+        Intent i = new Intent(main.this , mapView.class);
+        startActivity(i);
+    }
+
+    public void add_new_company(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.company_i);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.add_co_page);
+        if(old_layout.getVisibility()==view.VISIBLE){
+            old_layout.setVisibility(view.GONE);
+            new_layout.setVisibility(view.VISIBLE);
+        }
+    }
+    //����������
+    public void add_co_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.add_co_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.company_i);
+        old_layout.setVisibility(view.GONE);
+        new_layout.setVisibility(view.VISIBLE);
+    }
+    public void add_co_page_save_and_new(View view){
+        EditText company_name = (EditText) findViewById(R.id.xjgs_gsm_input);
+        EditText tel = (EditText) findViewById(R.id.xjgs_lxdh_input);
+        EditText addr = (EditText) findViewById(R.id.xjgs_dz_input);
+        EditText web = (EditText) findViewById(R.id.xjgs_wz_input);
+        EditText field = (EditText) findViewById(R.id.xjgs_hy_input);
+        ArrayList<CompanyInfo> currentCompanyList = new ArrayList<CompanyInfo>();
+        CompanyInfo currentCompany = new CompanyInfo();
+        currentCompany.setName(company_name.getText().toString());
+        currentCompany.setPhone(tel.getText().toString());
+        currentCompany.setAddress(addr.getText().toString());
+        currentCompany.setWebsite(web.getText().toString());
+        currentCompany.setField(field.getText().toString());
+
+        currentCompanyList.add(currentCompany);
+
+        ContactDBManager.add_company(currentCompanyList);
+        LinearLayout old_layout = (LinearLayout) findViewById(R.id.add_co_page);
+        LinearLayout new_layout = (LinearLayout) findViewById(R.id.company_i);
+        old_layout.setVisibility(view.GONE);
+        new_layout.setVisibility(view.VISIBLE);
+    }
+
+    //�����༭��˾ҳ
+    public void Turn_to_edit_co_page(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.company_i);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.edit_co_page);
+        old_layout.setVisibility(view.GONE);
+        new_layout.setVisibility(view.VISIBLE);
+    }
+    //����������
+    public void edit_co_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.edit_co_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.company_i);
+        old_layout.setVisibility(view.GONE);
+        new_layout.setVisibility(view.VISIBLE);
+    }
+
+
+    //չ���رյ�ַ��Ϣ��������ַ���ջ���ַ��
+    public void address_list_change(View view) {
+        LinearLayout ly = (LinearLayout)findViewById(R.id.dzxx_fo);
+        if(ly.getVisibility() == View.VISIBLE){
+            ly.setVisibility(View.GONE);
+        }
+        else {
+            ly.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void Turn_to_kddz_page(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.company_i);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.kddz_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+    //·µ»ØÖ÷½çÃæ
+    public void Kddz_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.kddz_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.company_i);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+    //ÌøÖÁ¿ªµ¥µØÖ·±à¼­Ò³
+    public void Turn_to_edit_kddz_page(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.kddz_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.edit_kddz_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+    //·µ»Ø¿ªµ¥µØÖ·²é¿´Ò³
+    public void edit_kddz_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.edit_kddz_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.kddz_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+    //±£´æ±à¼­¿ªµ¥µØÖ·
+    public void edit_kddz_page_save(View view) {
+
+    }
+
+    //ÌøÖÁÊÕ»õµØÖ·²é¿´Ò³
+    public void Turn_to_shdz_page(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.company_i);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.shdz_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+
+    //·µ»ØÖ÷½çÃæ
+    public void Shdz_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.shdz_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.company_i);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+    //ÌøÖÁÊÕ»õµØÖ·±à¼­Ò³
+    public void Turn_to_edit_shdz_page(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.shdz_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.edit_shdz_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+
+    //·µ»ØÊÕ»õµØÖ·²é¿´Ò³
+    public void edit_shdz_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.edit_shdz_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.shdz_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+
+    public void edit_shdz_page_save(View view) {
+
+    }
+    //µØÖ·ÐÅÏ¢¼°Ïà¹Ø   end------------------------------------------------------------------
+
+    //ҵ����
+    //�����½�ҵ����ҳ
+    public void Turn_to_add_ywjc_page(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.company_i);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.add_ywjc_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+
+    //����������
+    public void Ywjc_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.add_ywjc_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.company_i);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+
+    //ҵ���̱���
+    public void Ywjc_save(View view) {
+
+    }
+
+    //���ʷ
+    //�����½����ʷҳ
+    public void Turn_to_add_hdls_page(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.company_i);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.add_hdls_page);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+
+    //����������
+    public void Hdls_page_back(View view) {
+        LinearLayout old_layout = (LinearLayout)findViewById(R.id.add_hdls_page);
+        LinearLayout new_layout = (LinearLayout)findViewById(R.id.company_i);
+        old_layout.setVisibility(View.GONE);
+        new_layout.setVisibility(View.VISIBLE);
+    }
+
+    //���ʷ����
+    public void Hdls_save(View view) {
+
+    }
+
 
     @Override
     protected void onPause() {
@@ -101,12 +306,13 @@ public class main extends Activity {
 
     @Override
     protected void onResume() {
-        refreshList();
+        //refreshList();
         super.onResume();
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        ContactDBManager.company_deleteAll();
         ContactDBManager.closeDB();
     }
 
